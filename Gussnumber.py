03e76e5ab4dc6e58a39c020d265b7b0fcbd62d63
adil patel <adilazeempatel2005@gmail.com>
2026-07-09 01:27:01 +0530

    Add files via upload
    Add Number Guessing Game project

---
+++ Gussnumber.py
@@ -0,0 +1,23 @@
+import random
+
+num=random.randint(1,10)
+tries=0
+
+while True:
+    guess=int(input("pleass guess your number :"))
+    if guess==num:
+        tries+=1
+        print(f"you are right your  guessed the number is {tries} tries")
+        break
+    
+    elif num < guess:
+        tries+=1
+        print("lower number")
+
+    elif num > guess:
+        tries+=1
+        print("higher number")
+        
+    else:
+        tries+=1
+        print("sorry your number wrong")
